92b080ff01cc490436c4f74242d4d85d2e188c0e
Rayhan Chowdhury <rayhan.chowdhury@kcl.ac.uk>
2021-10-30 18:59:05 +0100

    Updated pass and fail test and updated README

--- README.md
+++ README.md
@@ -4,5 +4,10 @@ Test Engineer II - Take Home Task
 
 
 
-To run a test
+To run a test in the terminal
+1. Do npx cypress run
+
+
+To run a test via the Cypress GUI
 1. Do npx cypress open
+2. Choose which browser to execute the tests in
--- cypress/integration/test/test1.spec.js
+++ cypress/integration/test/test1.spec.js
@@ -1,16 +1,31 @@
-describe("Pass restaurant test", function(){
+describe("VoucherCodes Test Engineer Task", function(){
 
-    it('Search for restaurant', function() {
+    it('Search for offers in Kentish Town', function() {
         cy.visit("https://www.vouchercodes.co.uk/restaurant-vouchers.html")
         cy.get('[placeholder="Town or postcode"]')
-        .type('London')
+        .type('Kentish Town')
+        .should('have.value', 'Kentish Town')
         cy.get('[name="day-select"]')
         .select('Today')
+        .contains('Today')
         cy.get('[name="people-select"]')
         .select('5')
+        .contains('5')
         cy.get('[data-qa="el:findRestaurantsVoucherButton"]')
+        .contains('Find restaurants vouchers')
         .click()
   })
+
+  it('Assert offers search query', function() {
+    
+    cy.get('[placeholder="Town or postcode"]')
+    .should('have.value', 'Kentish Town')
+    cy.get('[data-qa="el:dayDropdown"]', { force: true })
+    .contains('Today')
+    cy.get('[name="people-select"]')
+    .contains('5')
+})
+
 
 
 
